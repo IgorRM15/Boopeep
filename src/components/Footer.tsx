57f17e1e7fdb537c84e042e6bd8f263620640246
IgorRM15 <i.rocha.martins10@gmail.com>
2024-06-08 08:59:43 -0300

    navbar1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { FaGithub, FaInstagram } from 'react-icons/fa';
-import '../Css/Footer.css';
+import './Css/Footer.css';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <div className="social-links">
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-          <FaGithub size={24} color="#fff" />
+      <div className="footer-container">
+        <a href="https://github.com/IgorRM15" target="_blank" rel="noopener noreferrer">
+          <FaGithub size={30} color="white" />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagram size={24} color="#fff" />
+        <a href="https://instagram.com/codetatio" target="_blank" rel="noopener noreferrer">
+          <FaInstagram size={30} color="white" />
         </a>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
