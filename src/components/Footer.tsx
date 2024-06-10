873152e1327535e7c47f5f5cdf41abd7d738adba
unknown <IgorRM15>
2024-06-10 19:47:49 -0300

    atualizaçao da navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { FaGithub, FaInstagram } from 'react-icons/fa';
-import './Css/Footer.css';
+import '../Css/Footer.css';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
         <a href="https://github.com/IgorRM15" target="_blank" rel="noopener noreferrer">
-          <FaGithub size={30} color="white" />
+          <FaGithub size={30} className="social-icon" />
         </a>
         <a href="https://instagram.com/codetatio" target="_blank" rel="noopener noreferrer">
-          <FaInstagram size={30} color="white" />
+          <FaInstagram size={30} className="social-icon" />
         </a>
       </div>
     </footer>
